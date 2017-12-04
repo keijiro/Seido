868e10ede7065d8f724e1d30bbe9f4adccb91ac3
Keijiro Takahashi <keijiro@gmail.com>
2017-12-05 01:18:05 +0900

    Added Klak and Lasp.

---
+++ Assets/Lasp/Internal/PluginEntry.cs
@@ -0,0 +1,67 @@
+﻿// LASP - Low-latency Audio Signal Processing plugin for Unity
+// https://github.com/keijiro/Lasp
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Lasp
+{
+    public enum FilterType { Bypass, LowPass, BandPass, HighPass }
+
+    public static class PluginEntry
+    {
+        #region Plugin interface
+
+        [DllImport("Lasp", EntryPoint="LaspCreateDriver")]
+        public static extern IntPtr CreateDriver();
+
+        [DllImport("Lasp", EntryPoint="LaspDeleteDriver")]
+        public static extern void DeleteDriver(IntPtr driver);
+
+        [DllImport("Lasp", EntryPoint="LaspOpenStream")]
+        public static extern bool OpenStream(IntPtr driver);
+
+        [DllImport("Lasp", EntryPoint="LaspCloseStream")]
+        public static extern void CloseStream(IntPtr driver);
+
+        [DllImport("Lasp", EntryPoint="LaspGetSampleRate")]
+        public static extern float GetSampleRate(IntPtr driver);
+
+        [DllImport("Lasp", EntryPoint="LaspGetPeakLevel")]
+        public static extern float GetPeakLevel(IntPtr driver, FilterType filter, float duration);
+
+        [DllImport("Lasp", EntryPoint="LaspCalculateRMS")]
+        public static extern float CalculateRMS(IntPtr driver, FilterType filter, float duration);
+
+        [DllImport("Lasp", EntryPoint="LaspRetrieveWaveform")]
+        public static extern int RetrieveWaveform(IntPtr driver, FilterType filter, float[] dest, int length);
+
+        #endregion
+
+        #region Debug helpers
+
+        public static void SetupLogger()
+        {
+            var del = (PrintDelegate)Log;
+            var ptr = Marshal.GetFunctionPointerForDelegate(del);
+            ReplaceLogger(ptr);
+        }
+
+        [DllImport("Lasp", EntryPoint="LaspReplaceLogger")]
+        public static extern void ReplaceLogger(IntPtr logger);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void PrintDelegate(string message);
+
+        static void Log(string message)
+        {
+        #if UNITY_EDITOR
+            UnityEngine.Debug.Log(message);
+        #else
+            System.Console.WriteLine(message);
+        #endif
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/WallFx/SlitLines.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Seido
+{
+    [ExecuteInEditMode]
+    public class SlitLines : MonoBehaviour
+    {
+        [SerializeField] Color _color1 = Color.black;
+        [SerializeField] Color _color2 = Color.white;
+        [SerializeField] float _frequency = 10;
+        [SerializeField] float _speed = 1;
+        [SerializeField] float _width = 0.1f;
+
+        public float width {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        [SerializeField, HideInInspector] Shader _shader;
+
+        Material _material;
+        float _time;
+
+        void OnDestroy()
+        {
+            if (_material != null)
+            {
+                if (Application.isPlaying)
+                    Destroy(_material);
+                else
+                    DestroyImmediate(_material);
+            }
+        }
+
+        void Update()
+        {
+            if (Application.isPlaying) _time += Time.deltaTime * _speed;
+        }
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (_material == null)
+            {
+                _material = new Material(_shader);
+                _material.hideFlags = HideFlags.DontSave;
+            }
+
+            _material.SetColor("_Color1", _color1);
+            _material.SetColor("_Color2", _color2);
+            _material.SetFloat("_LocalTime", _time);
+            _material.SetFloat("_Frequency", _frequency);
+            _material.SetFloat("_Width", _width);
+
+            Graphics.Blit(source, destination, _material, 0);
+        }
+    }
+}
