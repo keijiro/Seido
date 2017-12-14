e00e8d356da186e1c309675374b43b13fbb58b0b
Keijiro Takahashi <keijiro@gmail.com>
2017-12-14 11:00:14 +0900

    Condifurator

---
+++ Assets/Configurator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Seido
+{
+    class Configurator : MonoBehaviour
+    {
+        void Start()
+        {
+            // Hide cursor at runtime.
+            #if !UNITY_EDITOR
+            Cursor.visible = false;
+            #endif
+
+            // Try to activate the last two displays.
+            var displayCount = Display.displays.Length;
+            if (displayCount > 1)
+            {
+                Display.displays[displayCount - 1].Activate();
+                Display.displays[displayCount    ].Activate();
+            }
+        }
+    }
+}
