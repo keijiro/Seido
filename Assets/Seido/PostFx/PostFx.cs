f452865da47b2c3fc3e15fb944d6b5fd1935640c
Keijiro Takahashi <keijiro@gmail.com>
2017-12-16 21:33:35 +0900

    gradient controller

--- Assets/Seido/PostFx/PostFx.cs
+++ Assets/Seido/PostFx/PostFx.cs
@@ -13,13 +13,31 @@ namespace Seido
         [SerializeField] float _gradientFrequency = 1;
         [SerializeField] float _gradientSpeed = 1;
 
+        public CosineGradient gradient {
+            get { return _gradient; }
+            set { _gradient = value; }
+        }
+
         #endregion
 
         #region Private variables
 
         [SerializeField, HideInInspector] Shader _shader;
         Material _material;
+
+        Vector4 _gCoeffsA, _gCoeffsB, _gCoeffsC2, _gCoeffsD2;
         float _time;
+
+        void InitializeGCoeffs()
+        {
+            if (_gradient != null)
+            {
+                _gCoeffsA = _gradient.coeffsA;
+                _gCoeffsB = _gradient.coeffsB;
+                _gCoeffsC2 = _gradient.coeffsC2;
+                _gCoeffsD2 = _gradient.coeffsD2;
+            }
+        }
 
         #endregion
 
@@ -33,10 +51,33 @@ namespace Seido
                 DestroyImmediate(_material);
         }
 
+        void Start()
+        {
+            InitializeGCoeffs();
+        }
+
         void Update()
         {
             if (Application.isPlaying)
-                _time += Time.deltaTime * _gradientSpeed;
+            {
+                var dt = Time.deltaTime;
+                _time += _gradientSpeed * dt;
+
+                if (_gradient != null)
+                {
+                    var exp = Mathf.Exp(-0.5f * dt);
+                    _gCoeffsA = Vector4.Lerp(_gradient.coeffsA, _gCoeffsA, exp);
+                    _gCoeffsB = Vector4.Lerp(_gradient.coeffsB, _gCoeffsB, exp);
+                    _gCoeffsC2 = Vector4.Lerp(_gradient.coeffsC2, _gCoeffsC2, exp);
+                    _gCoeffsD2 = Vector4.Lerp(_gradient.coeffsD2, _gCoeffsD2, exp);
+                }
+            }
+            else
+            {
+                _time = 1;
+
+                InitializeGCoeffs();
+            }
         }
 
         void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -47,14 +88,10 @@ namespace Seido
                 _material.hideFlags = HideFlags.DontSave;
             }
 
-            if (_gradient != null)
-            {
-                _material.SetVector("_GradientA", _gradient.coeffsA);
-                _material.SetVector("_GradientB", _gradient.coeffsB);
-                _material.SetVector("_GradientC", _gradient.coeffsC2);
-                _material.SetVector("_GradientD", _gradient.coeffsD2);
-            }
-
+            _material.SetVector("_GradientA", _gCoeffsA);
+            _material.SetVector("_GradientB", _gCoeffsB);
+            _material.SetVector("_GradientC", _gCoeffsC2);
+            _material.SetVector("_GradientD", _gCoeffsD2);
             _material.SetFloat("_Frequency", _gradientFrequency);
             _material.SetFloat("_LocalTime", _time);
 
--- Assets/Seido/Scripts/SceneController.cs
+++ Assets/Seido/Scripts/SceneController.cs
@@ -1,10 +1,26 @@
 ﻿using UnityEngine;
+using Klak.Chromatics;
 
 namespace Seido
 {
     class SceneController : MonoBehaviour
     {
+        [SerializeField] CosineGradient[] _gradients;
         [SerializeField] GameObject[] _videoPlayers;
+
+        PostFx _postFx;
+
+        static readonly KeyCode[] _gradientKeys = {
+            KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.T,
+            KeyCode.Y, KeyCode.U, KeyCode.I, KeyCode.O, KeyCode.P
+        };
+
+        bool CheckVideoPlayerActive()
+        {
+            foreach (var go in _videoPlayers)
+                if (go.activeInHierarchy) return true;
+            return false;
+        }
 
         void Start()
         {
@@ -20,15 +36,28 @@ namespace Seido
                 Display.displays[displayCount - 1].Activate();
                 Display.displays[displayCount    ].Activate();
             }
+
+            _postFx = FindObjectOfType<PostFx>();
         }
 
         void Update()
         {
+            if (CheckVideoPlayerActive()) return;
+
             for (var i = 0; i < _videoPlayers.Length; i++)
             {
                 if (Input.GetKeyDown(KeyCode.F1 + i))
                 {
                     _videoPlayers[i].SetActive(true);
+                    break;
+                }
+            }
+
+            for (var i = 0; i < _gradients.Length; i++)
+            {
+                if (Input.GetKeyDown(_gradientKeys[i]))
+                {
+                    _postFx.gradient = _gradients[i];
                     break;
                 }
             }
