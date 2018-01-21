81ad198084013488be324e2df3be845e098e5f9f
Keijiro Takahashi <keijiro@gmail.com>
2018-01-22 00:07:29 +0900

    VideoWall: Changed to use quad renderer to present video.

--- Assets/Seido/VideoWall/VideoWall.cs
+++ Assets/Seido/VideoWall/VideoWall.cs
@@ -7,20 +7,12 @@ namespace Seido
     public class VideoWall : MonoBehaviour
     {
         [SerializeField] VideoClip _clip;
-        [SerializeField] Camera _targetCamera;
         [SerializeField] float[] _cuePoints = new float[] { 0 };
-        [SerializeField] Vector2 _repeat = Vector2.one;
-        [SerializeField] Vector2 _scroll = Vector2.zero;
-
-        [HideInInspector, SerializeField] Shader _shader;
+        [SerializeField] Transform _wallRoot;
 
         VideoPlayer _player;
-        Vector2 _offset;
-        float _opacity;
-        bool _closing;
-
-        Material _material;
-        CommandBuffer _blitCommand;
+        Renderer[] _renderers;
+        MaterialPropertyBlock _sheet;
 
         void OnEnable()
         {
@@ -38,70 +30,46 @@ namespace Seido
             _player.clip = _clip;
             _player.Prepare();
 
-            // Set up the blit shader.
-            _material = new Material(_shader);
+            // Enumerate the wall renderers.
+            _renderers = _wallRoot.GetComponentsInChildren<Renderer>();
 
-            // Add the blit command to the target camera.
-            _blitCommand = new CommandBuffer();
-            _blitCommand.name = gameObject.name;
-            _blitCommand.Blit(_player.texture, BuiltinRenderTextureType.CameraTarget, _material, 0);
-
-            if (_targetCamera != null)
-                _targetCamera.AddCommandBuffer( CameraEvent.AfterImageEffects, _blitCommand);
-
-            // Start fading in.
-            _opacity = 0;
-            _closing = false;
+            // Initialize the material sheet.
+            _sheet = new MaterialPropertyBlock();
         }
 
         void OnDisable()
         {
-            // Remove the blit command from the target camera.
-            if (_targetCamera != null)
-                _targetCamera.RemoveCommandBuffer(CameraEvent.AfterImageEffects, _blitCommand);
-
-            _blitCommand.Release();
-            _blitCommand = null;
-
-            // Destroy the blit shader material.
-            Destroy(_material);
-            _material = null;
-
             // Stop playing back and destroy the video player.
             _player.Stop();
             Destroy(_player);
             _player = null;
+
+            // Hide the wall renderers.
+            foreach (var r in _renderers) if (r != null) r.enabled = false;
+
+            _renderers = null;
+            _sheet = null;
         }
 
         void Update()
         {
-            // Start closing when pressing down the Q key.
-            if (Input.GetKeyDown(KeyCode.Q)) _closing = true;
-
-            if (_closing)
+            // Close if the Q key was pressed down.
+            if (Input.GetKeyDown(KeyCode.Q))
             {
-                // Fade out and disable itself at the end.
-                _opacity -= Time.deltaTime;
-                if (_opacity <= 0)
+                gameObject.SetActive(false);
+                return;
+            }
+
+            // Update the wall renderers (only when the video texture is ready).
+            if (_player.texture != null)
+            {
+                _sheet.SetTexture("_MainTex", _player.texture);
+                foreach (var r in _renderers)
                 {
-                    gameObject.SetActive(false);
-                    return;
+                    r.enabled = true;
+                    r.SetPropertyBlock(_sheet);
                 }
             }
-            else
-            {
-                // Fade in
-                _opacity = Mathf.Clamp01(_opacity + Time.deltaTime);
-            }
-
-            // Tile scrolling
-            _offset += Time.deltaTime * _scroll;
-
-            // Update the material properties.
-            _material.SetVector("_Repeat", _repeat);
-            _material.SetVector("_Offset", _offset);
-            _material.SetTexture("_MainTex", _player.texture);
-            _material.SetFloat("_Opacity", _opacity);
 
             // Playback start
             if (Input.GetKeyDown(KeyCode.Space))
